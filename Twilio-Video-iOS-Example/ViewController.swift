988586d1dd15b20e043041f1315eba8f1337e15a
Sam Agnew <sagnew@eden.rutgers.edu>
2016-02-12 15:43:30 -0500

    Initialize video client

--- Twilio-Video-iOS-Example/ViewController.swift
+++ Twilio-Video-iOS-Example/ViewController.swift
@@ -7,17 +7,49 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    var accessToken = "TWILIO_ACCESS_TOKEN"
+    var tokenUrl = "http://localhost:8000/token.php"
+    
+    var accessManager: TwilioAccessManager?
+    var client: TwilioConversationsClient?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Configure access token
+        if accessToken == "TWILIO_ACCESS_TOKEN" {
+            Alamofire.request(.GET, self.tokenUrl)
+                .responseJSON(completionHandler: { response in
+                    if let JSON = response.result.value {
+                        if let token = JSON["token"] {
+                            self.accessToken = token as! String
+                            print(self.accessToken)
+                        }
+                    }
+                })
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func initializeClient() {
+        // create an access manager to take care of token expiration events
+        self.accessManager = TwilioAccessManager(token:self.accessToken, delegate:self);
+        
+        self.client = TwilioConversationsClient(accessManager: self.accessManager!,
+            delegate: self);
+        
+        // Start listening for invitations - delegate methods are fired when the
+        // connection succeeds or fails
+        self.client?.listen();
+        print("The client identity is \(self.client?.identity)");
     }
 
 
